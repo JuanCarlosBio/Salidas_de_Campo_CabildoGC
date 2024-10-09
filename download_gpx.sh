2b3cb2abf4db82dbfddc214038642a393fb9a9f0
Juan Carlos García Estupiñán <jcge97@gmail.com>
2024-10-09 17:58:04 +0200

    Añadir la salida de campo del 9/10/2024, hay que trabajar más este proyecto

--- download_gpx.sh
+++ download_gpx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-dropbox="https://www.dropbox.com/scl/fo/8z94chsvow2jdsiqp671y/AE_En7cxUwPiObSOGeiH8vk?rlkey=lti74t6t38wog9h9uo3v2wzqk&st=6xvo4a6g&dl=0"
+dropbox="https://www.dropbox.com/scl/fo/8z94chsvow2jdsiqp671y/AE_En7cxUwPiObSOGeiH8vk?rlkey=lti74t6t38wog9h9uo3v2wzqk&st=n17xps6h&dl=0"
 
 if [[ ! -d track_data ]];then
   mkdir track_data
@@ -12,8 +12,10 @@ else
   echo "El archivo ZIP ya se encuentra descargado"
 fi
 
+wget -P track_data -O track_data/salidas_campo_gpx.zip $dropbox
+
 if [[ -f  track_data/salidas_campo_gpx.zip ]];then
-  unzip track_data/salidas_campo_gpx.zip -d track_data/ -u
+  unzip track_data/salidas_campo_gpx.zip -d track_data/ 
   rm track_data/salidas_campo_gpx.zip 
 else
   echo "El archivo ZIP ya se había descomprimido"
